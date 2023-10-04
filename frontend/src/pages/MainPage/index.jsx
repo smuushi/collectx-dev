c6e2fffedfa3c3c3a4db0ee48017abc1bb94c0d3
Aokfine <43547275+YaoAnthony@users.noreply.github.com>
2023-10-04 14:28:07 -0400

    add username display

--- frontend/src/pages/MainPage/index.jsx
+++ frontend/src/pages/MainPage/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
 const MainPage = () => {
+<<<<<<< Updated upstream
   return (
     <div>MainPage</div>
+=======
+
+  let authStatus = useSelector(state => state.auth)
+
+  return (
+    <>
+      <div>MainPage</div>
+
+      {authStatus.isLoggedIn? 
+        <h2>Congrats {authStatus.currentUser.name} are signed in</h2> 
+        : 
+        <h1>Not Logged In</h1>
+      }
+    </>
+
+>>>>>>> Stashed changes
   )
 }
 
