55702f19b2ffef3a2d780a875f5836d646bb29dd
Kelly Zhou <yinglinzhou0@gmail.com>
2023-12-01 15:06:17 -0500

    about link

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -330,11 +330,11 @@ const DesktopHeader = () => {
       
         <hr />
         <div className={`${pageSettings.padding} mt-5 flex justify-around `}>
-          {category.map((cat,index) => (
+          {/* {category.map((cat,index) => (
             <NavLink to={`/search/?search=${cat.name}`} key={index}>
                 <h3 className='text-md tracking-wide font-semibold select-none cursor-pointer'>{cat.name}</h3>
             </NavLink>
-          ))}
+          ))} */}
         </div>
       </motion.div>
     </header>
